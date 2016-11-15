33278864da5913d985308fa206be7e9ff4c94997
kwhitejr <kwhitejr.edu@gmail.com>
2016-11-14 18:48:55 -1000

    finished q 1

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ window.onload = function(){
 //Example
 var el = document.createElement('h1'); //creating a "h1" element (h1 heading)
 el.id = "headline"; //creating an identifier for the 'h1' element
-el.innerHTML = "Welcome to Tacocat's internet page."; //adding content to the h1 element
+el.innerHTML = "Welcome to Josh's internet page."; //adding content to the h1 element
 container.appendChild(el); //adds the h1 element to the container div element.
 
 //Exercise 1: 
@@ -14,13 +14,16 @@ Next, add the following text inside this newly created 'div' element:
 
 Append this 'div' element inside the "bigBox" div element that has already been created in the index.html file*/
 
-
+// var div = 'div';
 /*Helpful Hints:
 document.createElement creates an Element node
 element.id sets or returns the identifier of an element
 element.innerHTML sets or returns the HTML element content of an element
 node.appendChild() is a method that appends a node as the last child of a node*/
-
+var catBox = document.createElement('div');
+catBox.id = 'meow';
+catBox.innerHTML = 'Overcome its fear of cucumbers!';
+bigBox.appendChild(catBox);
 
 //Exercise 2
 /*Declare a variable named catBox2 and create an element of your choice.  Give your element an id of "purr".  Next assign your element with the following text:
